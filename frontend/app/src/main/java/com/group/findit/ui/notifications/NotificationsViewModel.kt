7cce94f64db600540bc17f25c4625185a724a17b
vicente-tenconi <tenconivini@gmail.com>
2025-03-22 13:42:14 +0100

    Agregando archivos faltantes

---
+++ frontend/app/src/main/java/com/group/findit/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,10 @@
+package com.group.findit.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+
+}
