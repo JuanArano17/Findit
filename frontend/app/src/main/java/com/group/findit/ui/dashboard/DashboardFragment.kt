ffbbe86860ca8b4a2b0a93f87128ecfde914b026
vanessamachordom <vane_divis@hotmail.es>
2025-03-28 13:06:50 +0100

    Avances en interfaz de dashboard

--- frontend/app/src/main/java/com/group/findit/ui/dashboard/DashboardFragment.kt
+++ frontend/app/src/main/java/com/group/findit/ui/dashboard/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.group.findit.R
 import com.group.findit.databinding.FragmentDashboardBinding
@@ -28,7 +29,17 @@ class DashboardFragment: Fragment() {
             .load(R.drawable.download)
             .into(binding.gifBackgroundDashboard)
 
+        setupRecyclerView()
         return root
+    }
+
+    private fun setupRecyclerView() {
+        val nombres = listOf("Maria","Vane", "Juan", "Vini", "Julian")
+        val puntajes = listOf(176,154, 75, 48, 20)
+        val adapter = ScoreAdapter(nombres, puntajes)
+        binding.recyclerViewScores.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewScores.adapter = ScoreAdapter(nombres, puntajes)
+        adapter.notifyDataSetChanged()
     }
 
     override fun onDestroyView() {
---
+++ frontend/app/src/main/java/com/group/findit/ui/dashboard/ScoreAdapter.kt
@@ -0,0 +1,32 @@
+package com.group.findit.ui.dashboard
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.group.findit.R
+
+class ScoreAdapter(
+    private val nombres: List<String>,
+    private val puntajes: List<Int>
+) : RecyclerView.Adapter<ScoreAdapter.ScoreViewHolder>() {
+
+    class ScoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nombreJugador: TextView = itemView.findViewById(R.id.textNombre)
+        val puntajeJugador: TextView = itemView.findViewById(R.id.textPuntaje)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScoreViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_score, parent, false)
+        return ScoreViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScoreViewHolder, position: Int) {
+        holder.nombreJugador.text = nombres[position]
+        holder.puntajeJugador.text = puntajes[position].toString()
+    }
+
+    override fun getItemCount(): Int = nombres.size
+}
