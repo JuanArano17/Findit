6addb8840a281a61fa61a287af8a6480fc2d721c
vanessamachordom <vane_divis@hotmail.es>
2025-05-05 19:03:48 +0200

    arreglos

--- frontend/app/src/main/java/com/group/findit/ui/data/game/ApiClient.kt
+++ frontend/app/src/main/java/com/group/findit/ui/data/game/ApiClient.kt
@@ -4,9 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    //private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
     //private const val BASE_URL = "http://192.168.1.114:8000"
-    private const val BASE_URL = "http://juop12:8000/"
+    //private const val BASE_URL = "http://juop12:8000/"
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
